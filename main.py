72ff9af81113a0ee479a7c1e2bf0e6ed0db387dd
capyxander <exby.aitch@gmail.com>
2025-02-22 18:59:34 +0000

    comments added

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ def main(msg):
     print(msg)
 main("kevin the goblin looks wearily at the rabid goblin" )
 print("The rabid goblin bites him on the nose")
+//Twist: rabid goblin is really Francois goblin kevin's long lost  twin brother 
